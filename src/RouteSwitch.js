3a50ce7b69d8dec75cb94cb4234417cfedddfeb7
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-13 14:45:28 +0200

    Fix route nesting

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,15 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Homepage from "./components/Homepage";
 
 function RouteSwitch() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route index element={<Homepage />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<Homepage />} />
+        </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
