18ffbb3d1eebc03bf3be040d4380679bf33055e1
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-13 14:46:10 +0200

    Add signout

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import SignOut from "./SignOut";
 
 function Homepage() {
+  const [user] = useOutletContext();
   const [tweet, setTweet] = useState("");
 
   function handleInput(e) {
@@ -12,14 +15,18 @@ function Homepage() {
     console.log(tweet);
   }
 
-  return <main>
-    <form>
-      <label>Create Tweet
-        <input value={tweet} onChange={handleInput} />
-      </label>
-      <button onClick={handleTweet} >Tweet</button>
-    </form>
-  </main>;
+  return (
+    <main>
+      <form>
+        <label>
+          Create Tweet
+          <textarea value={tweet} onChange={handleInput} />
+        </label>
+        <button onClick={handleTweet}>Tweet</button>
+      </form>
+      <SignOut />
+    </main>
+  );
 }
 
 export default Homepage;
