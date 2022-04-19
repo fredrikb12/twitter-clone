24af6719063677bfdde5c84bae62cc3f6bb04e8f
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-19 17:40:22 +0200

    Display user info on profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,6 +14,7 @@ import TweetsFeed from "./TweetsFeed";
 
 function Profile() {
   const [user] = useOutletContext();
+  const [userInfo, setUserInfo] = useState({});
   const [tweets, setTweets] = useState([]);
   const { userTag } = useParams();
 
@@ -26,6 +27,17 @@ function Profile() {
       let data = [];
 
       qSnap.forEach((doc) => {
+        console.log(doc.data());
+        const userData = doc.data();
+        setUserInfo(() => {
+          return {
+            bio: userData.bio,
+            displayName: userData.displayName,
+            followers: userData.followers.length,
+            following: userData.following.length,
+            tweets: userData.tweets.length,
+          };
+        });
         const tweetData = doc.data().tweets.map((tweet) => {
           return {
             ...tweet,
@@ -46,11 +58,33 @@ function Profile() {
   }, [user.uid, userTag]);
 
   useEffect(() => {
+    async function loadUserInfo() {
+      const usersRef = collection(getDB(), "users");
+      const q = query(usersRef, where("tag", "==", userTag));
+      const qSnap = await getDocs(q);
+
+      qSnap.forEach((doc) => {});
+    }
+    loadUserInfo();
+  }, [userTag]);
+
+  useEffect(() => {
     console.log(tweets);
   }, [tweets]);
 
+  let renderObj = [];
+  if (!!userInfo) {
+    let data = [];
+    data.push(<h1>{userInfo.displayName}</h1>);
+    data.push(<p>{userInfo.bio}</p>);
+    data.push(<p>followers: {userInfo.followers}</p>);
+    data.push(<p>following: {userInfo.following}</p>);
+    renderObj = data;
+  }
+
   return (
     <div>
+      <div>{renderObj}</div>
       <TweetsFeed tweetData={tweets} />
     </div>
   );
