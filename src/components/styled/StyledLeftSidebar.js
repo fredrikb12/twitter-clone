485f3b4441915046fff89d3f9c40b70b325e1630
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-20 17:45:20 +0200

    Move sign-out button to bottom of container

--- src/components/styled/StyledLeftSidebar.js
+++ src/components/styled/StyledLeftSidebar.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledLeftSidebar = styled.header`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   width: 350px;
   min-height: 100px;
   background-color: chocolate;
