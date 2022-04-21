b6d1f4a65cfbdf5ff6220362144e6554855b0add
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-21 21:11:09 +0200

    Implement user info updating

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -13,10 +13,12 @@ import {
   where,
 } from "firebase/firestore";
 import { getDB } from "../firebase";
+import { RoundedImage } from "./styled/RoundedImage";
+import BarLoaderIcon from "./BarLoaderIcon";
 
 function Settings() {
   const [user] = useOutletContext();
-  const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(doc(getDB(), "users", user.uid), (doc) => {
@@ -32,7 +34,74 @@ function Settings() {
     console.log(userInfo);
   }, [userInfo]);
 
-  return <div></div>;
+  function handleChange(e) {
+    setUserInfo((prev) => {
+      const obj = { ...prev };
+      const name = e.target.name;
+      const value = e.target.value;
+
+      obj[name] = value;
+      return { ...obj };
+    });
+  }
+
+  async function submitInfo(e, field) {
+    const userRef = doc(getDB(), "users", user.uid);
+    const data = { ...userInfo };
+    await updateDoc(userRef, data);
+    console.log("updated");
+  }
+
+  return userInfo ? (
+    <form>
+      <div>
+        <label>
+          Edit Display Name
+          <input
+            onChange={handleChange}
+            name="displayName"
+            type="text"
+            value={userInfo.displayName}
+          />
+        </label>
+        <button type="button" onClick={(e) => submitInfo(e, "displayName")}>
+          Submit Name
+        </button>
+      </div>
+      <div>
+        <label>
+          Edit Bio
+          <textarea
+            onChange={handleChange}
+            name="bio"
+            type="text"
+            value={userInfo.bio}
+          />
+        </label>
+        <button type="button" onClick={(e) => submitInfo(e, "bio")}>
+          Submit Bio
+        </button>
+      </div>
+      <div>
+        <label>
+          Edit User Tag
+          <input
+            type="text"
+            name="tag"
+            onChange={handleChange}
+            value={userInfo.tag}
+          />
+        </label>
+        <button type="button" onClick={(e) => submitInfo(e, "tag")}>
+          Submit tag
+        </button>
+      </div>
+    </form>
+  ) : (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <BarLoaderIcon />
+    </div>
+  );
 }
 
 export default Settings;
