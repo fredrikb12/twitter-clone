b3cc45ec4f6c9286c4947f6f6cb9f912e82d91ae
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-21 21:06:53 +0200

    Add bar loading animation

---
+++ src/components/BarLoaderIcon.js
@@ -0,0 +1,13 @@
+import { StyledLoaderIcon } from "./styled/LoaderIcon.styled";
+
+function BarLoaderIcon() {
+  return (
+    <StyledLoaderIcon>
+      <div></div>
+      <div></div>
+      <div></div>
+    </StyledLoaderIcon>
+  );
+}
+
+export default BarLoaderIcon;
