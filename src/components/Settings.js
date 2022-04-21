98ff4baed08d4594f853e015be303396e654323e
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-21 20:00:32 +0200

    Fetch user info in settings component on load

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import {
+  arrayRemove,
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { getDB } from "../firebase";
 
 function Settings() {
+  const [user] = useOutletContext();
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(getDB(), "users", user.uid), (doc) => {
+      setUserInfo(() => {
+        return { ...doc.data() };
+      });
+    });
+
+    return () => unsubscribe();
+  }, [user]);
+
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
+
   return <div></div>;
 }
 
