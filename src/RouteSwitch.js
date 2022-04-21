50ad517453d3c8dc1b274f691fd9e517ca04f4c0
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-21 19:34:48 +0200

    Add settings route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,6 +2,7 @@ import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Homepage from "./components/Homepage";
 import Profile from "./components/Profile";
+import Settings from "./components/Settings";
 
 function RouteSwitch() {
   return (
@@ -10,6 +11,7 @@ function RouteSwitch() {
         <Route path="/" element={<App />}>
           <Route index element={<Homepage />} />
           <Route path="/profiles/:userTag" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
