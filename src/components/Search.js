dbba4900adb3e332b8cf6b293f8c10389e25f7c0
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-22 22:00:07 +0200

    Change conditional rendering and add styled link

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,10 +12,14 @@ import {
   whereField,
   getDocs,
 } from "firebase/firestore";
+import BarLoaderIcon from "./BarLoaderIcon";
+import { Link } from "react-router-dom";
+import { StyledLink } from "./styled/Link.styled";
 
 function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [displayResults, setDisplayResults] = useState(false);
 
   useEffect(() => {
@@ -27,6 +31,8 @@ function Search() {
   }
 
   async function handleSearch() {
+    setIsLoading(() => true);
+    setDisplayResults(() => true);
     const usersRef = collection(getDB(), "users");
     const tagQuery = query(usersRef, where("tag", "==", searchTerm));
     const nameQuery = query(usersRef, where("displayName", "==", searchTerm));
@@ -43,7 +49,37 @@ function Search() {
         }
       });
     });
+    setIsLoading(() => false);
     setSearchResults(() => [...data]);
+  }
+
+  let displayItem;
+
+  if (displayResults && searchResults.length === 0 && !isLoading) {
+    displayItem = (
+      <div style={{ padding: "10px 0px" }}>
+        <p style={{ paddingBottom: "10px" }}>
+          No users were found. If you expected a result, try again with proper
+          casing, as results are case sensitive.
+        </p>
+        <button
+          onClick={() => {
+            setDisplayResults(() => false);
+            setSearchResults(() => []);
+          }}
+        >
+          Close Search
+        </button>
+      </div>
+    );
+  } else if (isLoading) {
+    displayItem = <BarLoaderIcon />;
+  } else {
+    displayItem = (
+      <button type="button" onClick={handleSearch}>
+        Search
+      </button>
+    );
   }
 
   return (
@@ -53,30 +89,39 @@ function Search() {
         <input type="text" value={searchTerm} onChange={handleInput} />
       </label>
 
-      {searchResults.length > 0 ? (
-        searchResults.map((result) => {
-          return (
-            <div
-              style={{
-                border: "1px solid black",
-                padding: "10px",
-                position: "relative",
-              }}
-              key={result.tag}
-            >
-              <p style={{ fontSize: "1.3rem" }}>{result.displayName}</p>
-              <p style={{ fontSize: "0.9rem", color: "#1f1f1f" }}>
-                @{result.tag}
-              </p>
-              <button onClick={() => setSearchResults([])}>Close Search</button>
-            </div>
-          );
-        })
-      ) : (
-        <button type="button" onClick={handleSearch}>
-          Search
-        </button>
-      )}
+      {searchResults.length > 0 && displayResults
+        ? searchResults.map((result) => {
+            return (
+              <div
+                style={{
+                  border: "1px solid black",
+                  padding: "10px",
+                  position: "relative",
+                }}
+                key={result.tag}
+              >
+                <StyledLink as="a" textColor="#efefef" href={`/profiles/${result.tag}`}>
+                  <p style={{ fontSize: "1.3rem" }}>{result.displayName}</p>
+                </StyledLink>
+                <StyledLink
+                  as="a"
+                  textColor="#efefef"
+                  href={`/profiles/${result.tag}`}
+                >
+                  <p style={{ fontSize: "0.9rem" }}>@{result.tag}</p>
+                </StyledLink>
+                <button
+                  onClick={() => {
+                    setDisplayResults(() => false);
+                    setSearchResults(() => []);
+                  }}
+                >
+                  Close Search
+                </button>
+              </div>
+            );
+          })
+        : displayItem}
     </div>
   );
 }
