3d16e74541307e0b2a22f8c49ddcbabcdcc05967
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-25 13:07:54 +0200

    Remove unnecessary flex property

--- src/components/styled/StyledFlexColContainer.js
+++ src/components/styled/StyledFlexColContainer.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const StyledFlexColContainer = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1;
 `;
 
 export default StyledFlexColContainer;
--- src/components/styled/StyledFlexRowContainer.js
+++ src/components/styled/StyledFlexRowContainer.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const StyledFlexRowContainer = styled.div`
   display: flex;
-  flex: 1;
 `;
 
 export default StyledFlexRowContainer;
