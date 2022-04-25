d645e439767c5b067da9cf88bc209665cc3b5bde
fredrikb12 <fredrik.backman12@gmail.com>
2022-04-25 14:12:50 +0200

    Fix fonts

--- src/components/styled/GlobalStyle.js
+++ src/components/styled/GlobalStyle.js
@@ -1,49 +1,44 @@
 import { createGlobalStyle } from "styled-components";
+import Black from "../../fonts/Roboto-Black.ttf";
+import Bold from "../../fonts/Roboto-Bold.ttf";
+import Light from "../../fonts/Roboto-Light.ttf";
+import Medium from "../../fonts/Roboto-Medium.ttf";
+import Regular from "../../fonts/Roboto-Regular.ttf";
+import Thin from "../../fonts/Roboto-Thin.ttf";
 
 const GlobalStyle = createGlobalStyle`
 
 @font-face {
-    font-family: 'Roboto';
-    src: url('Roboto-Bold.ttf') format('truetype');
-    font-weight: bold;
-    font-style: normal;
-}
-
-@font-face {
-    font-family: 'Roboto';
-    src: url('Roboto-Black.ttf') format('truetype');
+    font-family: "Roboto";
+    src: url(${Regular}) format("truetype");
+    font-weight: 400;
+  }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${Black}) format("truetype");
     font-weight: 900;
-    font-style: normal;
-}
-
-@font-face {
-    font-family: 'Roboto';
-    src: url('Roboto-Light.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${Bold}) format("truetype");
+    font-weight: 700;
+  }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${Light}) format("truetype");
     font-weight: 300;
-    font-style: normal;
-}
-
-@font-face {
-    font-family: 'Roboto';
-    src: url('Roboto-Medium.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${Medium}) format("truetype");
     font-weight: 500;
-    font-style: normal;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Roboto';
-    src: url('Roboto-Regular.ttf') format('truetype');
-    font-weight: normal;
-    font-style: normal;
-}
-
-@font-face {
-    font-family: 'Roboto';
-    src: url('Roboto-Thin.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${Thin}) format("truetype");
     font-weight: 100;
-    font-style: normal;
-}
+  }
+
 
   * {
     box-sizing: border-box;
